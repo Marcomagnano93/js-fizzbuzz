3faf55567e6eeac746a2563f6cee3094f6c32745
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-06 13:51:43 +0100

    ADD: JS: for loop 1 to 100

---
+++ js/app.js
@@ -0,0 +1,33 @@
+//stampa i numeri da 1 a 100
+//- crea una variabile n che indichi in numero di ripetizioni
+
+    let n = 100; //number
+
+//- crea il ciclo for in modo che la variabile i si ripeta n volte
+
+    for(let i = 0; i < 100; i++){
+
+//- assicurati che la variabile i inizi da 1 e finisca con 100
+    //- crea una variabile che contenga i + 1
+        
+        let nResult = i + 1; //number
+        
+        console.log(nResult)
+    }
+
+
+//per i multipli di 3 stampa “Fizz” al posto del numero 
+//- dividi il numero per 3
+//- se il risultato (x = numero % 3) è 0 allora è divisibile per 3
+//- sostituisci il numero x con "fizz"
+
+
+//per i multipli di 5 stampa “Buzz” al post del numero
+//- dividi il numero per 5
+//- se il risultato (y = numero % 5) è 0 allora è divisibile per 5
+//- sostituisci il numero y con "buzz"
+
+//Per i numeri che sono sia multipli di 3 che di 5 stampa “FizzBuzz” al posto del numero
+//- SE il numero è divisibile per 3 && per 5 stampa "fizzBuzz" al posto del numero
+
+//ALTRIMENTI stampa il numero normalmente
