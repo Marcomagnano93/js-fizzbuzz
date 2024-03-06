8006d23c5d3bbf79ed9f9805d46a155c42a234e3
Marcomagnano93 <marcog.magnano@gmail.com>
2024-03-06 14:16:11 +0100

    ADD: JS: if else: 3 option: fizz buzz fizzbuzz

--- js/app.js
+++ js/app.js
@@ -11,23 +11,43 @@
     //- crea una variabile che contenga i + 1
         
         let nResult = i + 1; //number
-        
-        console.log(nResult)
-    }
+
+        // console.log(nResult)
+    
 
 
-//per i multipli di 3 stampa “Fizz” al posto del numero 
-//- dividi il numero per 3
-//- se il risultato (x = numero % 3) è 0 allora è divisibile per 3
-//- sostituisci il numero x con "fizz"
-
-
+//per i multipli di 3 stampa “Fizz” al posto del numero
 //per i multipli di 5 stampa “Buzz” al post del numero
-//- dividi il numero per 5
-//- se il risultato (y = numero % 5) è 0 allora è divisibile per 5
-//- sostituisci il numero y con "buzz"
-
 //Per i numeri che sono sia multipli di 3 che di 5 stampa “FizzBuzz” al posto del numero
+//- dividi il numero per 3 e 5 e ottieni il resto all'interno di due variabili
+        let x = nResult % 3; //number
+        let y = nResult % 5; //number
+
+//- se il risultato (x = numero % 3) è 0 allora è divisibile per 3
+//- se il risultato (y = numero % 5) è 0 allora è divisibile per 5
+
+//-***RICORDA CHE IL CODICE VIENE LETTO A CASCATA***
+
 //- SE il numero è divisibile per 3 && per 5 stampa "fizzBuzz" al posto del numero
+        if(x === 0 && y === 0){
+
+            nResult = "FizzBuzz"; //string
+        }
+//- sostituisci il numero x con "fizz"
+        else if(x === 0 && y !== 0){
+
+            nResult = "Fizz"; //string
+        }
+//- sostituisci il numero y con "buzz"
+        else if(x !== 0 && y === 0){
+            
+            nResult = "Buzz"; //string
+        }
+
+
+
+
 
 //ALTRIMENTI stampa il numero normalmente
+      console.log(nResult)
+    }
